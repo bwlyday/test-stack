e5c4012bdc52e332dcab1eb46cb6b556c0dda2e1
Brandon Lyday <bwlyday@gmail.com>
2019-10-29 16:07:22 -0700

    renders a statement about John Doe's Pokemon

--- my-typescript-app/src/App.tsx
+++ my-typescript-app/src/App.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import PokemonSearch from './components/PokemonSearch';
 import './App.css';
+
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <h1>Hello World!</h1>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <h1>Hello World!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PokemonSearch name = "John Doe" numberOfPokemon = {5} />
     </div>
-  );
-}
-
-const App2: React.FC = () => {
-  return(
-    <h1>YEET!</h1>
   );
 }
 
